df94e77886cc17b9eac93d09755205705be80e5a
Marsheila26 <marsheillaputri09@gmail.com>
2024-10-21 09:29:41 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "./components/hero";
+import RiwayatPekerjaan from "./components/riwayatPekerjaan";
+import "./sheila-style.css";
+
+export default function CVonline() {
+  return (   
+    <section>
+      <Hero/>
+      <RiwayatPekerjaan/>
+    </section>
+  );
+}
+
